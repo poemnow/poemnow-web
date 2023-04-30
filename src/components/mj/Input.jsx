a6ef8c91edaaae8b1ca1a8e91f9fac5d971d74e8
andbeyond0331 <mjpark1329@naver.com>
2023-04-30 22:07:33 +0900

    feature: component - Atoms(Input) 추가

---
+++ src/components/mj/Input.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const InputStyle = styled.input``;
+
+function Input(props) {
+  return <InputStyle></InputStyle>;
+}
+
+export default Input;
