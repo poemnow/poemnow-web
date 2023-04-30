6c3037d856478743a0d5ea41f244e30accc2f7da
BaeYoungSuk <bysuk01@gmail.com>
2023-04-30 22:15:44 +0900

    feat: myPage, Profile, 공통 컴포넌트 구현

---
+++ src/components/Atoms/ErrorComponent.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const ErrorStyle = styled.div`
+  color: red;
+`;
+
+export default function ErrorComponent({ message }) {
+  return message ? <ErrorStyle>{message}</ErrorStyle> : null;
+}
