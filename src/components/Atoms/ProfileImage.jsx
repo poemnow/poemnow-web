e0f3b3254d40339a590e6361e905e2614e8e4792
BaeYoungSuk <bysuk01@gmail.com>
2023-04-30 22:15:44 +0900

    feat: myPage, Profile, 공통 컴포넌트 구현

---
+++ src/components/Atoms/ProfileImage.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const ProfileImageStyle = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+
+export default function ProfileImage({ src, alt, size }) {
+  return (
+    <ProfileImageStyle src={src} alt={alt} size={size}></ProfileImageStyle>
+  );
+}
