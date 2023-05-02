65cf5e664495863bf39fa3c352436300709adad9
Sangho <istrangeho@gmail.com>
2023-05-02 21:38:13 +0900

    feature: Icon 수정 및 Layout TabBar 컴포넌트 구현

--- src/components/Atoms/Icon.jsx
+++ src/components/Atoms/Icon.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const IconStyled = styled.div`
   width: 24px;
   height: 24px;
-  padding: ${({ theme }) => theme.padding.xs};
 `;
 
 export default function Icon({ children }) {
---
+++ src/components/Organisms/TabBar.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import Tab from "../Molecules/Tab";
+
+const TabBarContainer = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 64px;
+`;
+
+export default function TabBar({ TabList }) {
+  return (
+    <TabBarContainer>
+      {TabList &&
+        TabList.map((tab, index) => {
+          return (
+            <li key={index}>
+              <Tab matchWord={tab} index={2}>
+                {tab}
+              </Tab>
+            </li>
+          );
+        })}
+    </TabBarContainer>
+  );
+}
