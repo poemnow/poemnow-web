50a32b64af2e97e374e484b3b157347ddd5d2980
andbeyond0331 <mjpark1329@naver.com>
2023-05-02 21:38:49 +0900

    feature: Component - Organisms - BookCard, BookCardList 추가

---
+++ src/components/Organisms/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import Text from "../Atoms/Text";
+
+const FooterStyle = styled.div``;
+
+export default function FooterList() {
+  return (
+    <FooterStyle>
+      <Text />
+    </FooterStyle>
+  );
+}
