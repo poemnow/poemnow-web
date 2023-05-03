26c3ee680e89fb10969831e6b8d360f00ac558cf
MOON <ansghddnd12@naver.com>
2023-05-03 21:19:07 +0900

    [+] modify , WrtieOption

--- src/components/Organisms/WriteOption.jsx
+++ src/components/Organisms/WriteOption.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import styled from "styled-components";
-import H3 from "../Atoms/H3";
-import Text from "../Atoms/Text";
+import TagCard from "./Tagcard";
 
 const ToggleButton = styled.button`
   background-color: ${({ active }) => (active ? "blue" : "grey")};
@@ -13,10 +12,13 @@ const ToggleButton = styled.button`
   cursor: pointer;
 `;
 
-const WriteOptionStyled = styled.div``;
+const NameToggleButton = styled(ToggleButton)`
+  margin-top: 10px;
+`;
 
 const WriteOption = () => {
   const [isPublic, setIsPublic] = useState(true);
+  const [isAnonymous, setIsAnonymous] = useState(false);
 
   const handlePublicToggle = () => {
     setIsPublic(true);
@@ -26,25 +28,32 @@ const WriteOption = () => {
     setIsPublic(false);
   };
 
+  const handleNameToggle = () => {
+    setIsAnonymous(false);
+  };
+
+  const handleAnonymousToggle = () => {
+    setIsAnonymous(true);
+  };
+
   return (
-    <WriteOptionStyled>
-      <H3>발행 옵션</H3>
-      <Text>공개 설정</Text>
+    <div>
       <ToggleButton active={isPublic} onClick={handlePublicToggle}>
         공개
       </ToggleButton>
       <ToggleButton active={!isPublic} onClick={handlePrivateToggle}>
         비공개
       </ToggleButton>
-      <Text>작가 설정</Text>
-      <ToggleButton active={isPublic} onClick={handlePublicToggle}>
-        필명 표기
-      </ToggleButton>
-      <ToggleButton active={!isPublic} onClick={handlePrivateToggle}>
-        익명 표기
-      </ToggleButton>
-      <Text>태그 편집</Text>
-    </WriteOptionStyled>
+      <br />
+      <NameToggleButton active={!isAnonymous} onClick={handleNameToggle}>
+        필명
+      </NameToggleButton>
+      <NameToggleButton active={isAnonymous} onClick={handleAnonymousToggle}>
+        익명
+      </NameToggleButton>
+      <br />
+      <TagCard text="태그 이름" count={10} />
+    </div>
   );
 };
 
