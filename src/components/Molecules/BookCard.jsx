3ac1d99ac05bcb990fbe4c1b75a8cb2dfec79718
박현철 <wns882@naver.com>
2023-05-07 01:23:46 +0900

    feature : ModalMiddle, SearchCondition, bookcard css 완성

--- src/components/Molecules/BookCard.jsx
+++ src/components/Molecules/BookCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import H2 from "../Atoms/H2";
+import H5 from "../Atoms/H5";
 import Text from "../Atoms/Text";
 import ProfileImage from "../Atoms/ProfileImage";
 import Date from "../Atoms/Date";
@@ -8,55 +8,111 @@ import Icon from "../Atoms/Icon";
 import BookCover from "../Atoms/BookCover";
 
 const BookCardStyle = styled.div`
-  display: inline-flex;
-  div {
-    display: inline-flex;
-  }
+  display: flex;
+`;
+const BookCardLeftContainer = styled.div`
+  margin-left: 15px;
+  margin-right: 15px;
+  margin-top: 15px;
+  display: flex;
+  flex-direction: column;
+`;
+const BookCardrightContainer = styled.div`
+  margin-right: 15px;
+  margin-top: 15px;
+  display: flex;
+  flex-direction: column;
+`;
+const BookCardContent = styled.div`
+  color: gray;
+  font-size: 14px;
+  height: 63px; /* 3줄에 해당하는 높이 */
+  line-height: 1.5; /* 텍스트 줄 간격 */
+  overflow: hidden; /* 넘치는 부분 숨김 */
+  margin-top: 15px;
+`;
+const BookAuthorContent = styled.div`
+  color: gray;
+  font-size: 14px;
+  margin-top: 15px;
+`;
+const BookLikeContent = styled.div`
+  color: gray;
+  font-size: 11px;
+  margin-top: 15px;
+`;
+const BookCommentCnt = styled.div`
+  color: gray;
+  font-size: 11px;
+  display: inline-block;
+  margin-left: 3px;
+`;
+const Outer = styled.div`
+  width: 92px;
+  height: 128px;
+  background-color: ${(props) => props.outer_color};
+  display: flex;
+  justify-content: center; /* 가로 중앙 정렬 */
+  align-items: center; /* 세로 중앙 정렬 */
+`;
+const Inner = styled.div`
+  background-color: ${(props) => props.inner_color};
+  width: 52px;
+  height: 94px;
+`;
+const Created_at = styled.div`
+  color: gray;
+  font-size: 11px;
+  margin-top: 10px;
+  text-align: center;
 `;
 
-export default function BookCard({ book }) {
+const Border = styled.div`
+  border-top: 1px solid #ccc;
+  margin-left: 15px;
+  margin-right: 15px;
+  margin-top: 15px;
+`;
+
+export default function BookCard({
+  keyword,
+  title,
+  bookDetail,
+  userName,
+  likeCnt,
+  createdAt,
+  inner_color,
+  outer_color,
+}) {
   return (
-    <BookCardStyle>
-      <span>
-        <H2>{book.title}</H2>
-        <Text>{book.bookDetail}</Text>
-        <div>
-          <span>
-            <ProfileImage src={book.userProfileImage}></ProfileImage>
-          </span>
-          <span>
-            <Text>{book.userName}</Text>
-          </span>
-          <span>
-            <Date>{book.createdAt}</Date>
-          </span>
-        </div>
-        <div>
-          <span>
-            <Icon>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+    <>
+      <BookCardStyle>
+        <BookCardLeftContainer>
+          <H5 color={"black"}>{title}</H5>
+          <BookCardContent>{bookDetail}</BookCardContent>
+          <BookAuthorContent>{userName}</BookAuthorContent>
+          <BookLikeContent>
+            <Icon size="11px">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                width="100%"
+                height="100%"
+              >
                 <path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z" />
               </svg>
             </Icon>
-          </span>
-          <span>
-            <Text>{book.likeCnt}</Text>
-          </span>
-          <span>
-            <Icon>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                <path d="M123.6 391.3c12.9-9.4 29.6-11.8 44.6-6.4c26.5 9.6 56.2 15.1 87.8 15.1c124.7 0 208-80.5 208-160s-83.3-160-208-160S48 160.5 48 240c0 32 12.4 62.8 35.7 89.2c8.6 9.7 12.8 22.5 11.8 35.5c-1.4 18.1-5.7 34.7-11.3 49.4c17-7.9 31.1-16.7 39.4-22.7zM21.2 431.9c1.8-2.7 3.5-5.4 5.1-8.1c10-16.6 19.5-38.4 21.4-62.9C17.7 326.8 0 285.1 0 240C0 125.1 114.6 32 256 32s256 93.1 256 208s-114.6 208-256 208c-37.1 0-72.3-6.4-104.1-17.9c-11.9 8.7-31.3 20.6-54.3 30.6c-15.1 6.6-32.3 12.6-50.1 16.1c-.8 .2-1.6 .3-2.4 .5c-4.4 .8-8.7 1.5-13.2 1.9c-.2 0-.5 .1-.7 .1c-5.1 .5-10.2 .8-15.3 .8c-6.5 0-12.3-3.9-14.8-9.9c-2.5-6-1.1-12.8 3.4-17.4c4.1-4.2 7.8-8.7 11.3-13.5c1.7-2.3 3.3-4.6 4.8-6.9c.1-.2 .2-.3 .3-.5z" />
-              </svg>
-            </Icon>
-          </span>
-          <span>
-            <Text>{book.commentCnt}</Text>
-          </span>
-        </div>
-      </span>
-      <span>
-        <BookCover></BookCover>
-      </span>
-    </BookCardStyle>
+            <BookCommentCnt>{likeCnt}</BookCommentCnt>
+          </BookLikeContent>
+        </BookCardLeftContainer>
+        <BookCardrightContainer>
+          <Outer outer_color={outer_color}>
+            <Inner inner_color={inner_color}></Inner>
+          </Outer>
+          <Created_at>발행일 {createdAt}</Created_at>
+        </BookCardrightContainer>
+      </BookCardStyle>
+      <Border></Border>
+    </>
   );
 }
--- src/components/Molecules/ModalMiddle.jsx
+++ src/components/Molecules/ModalMiddle.jsx
@@ -1,10 +1,56 @@
 import styled from "styled-components";
 import Button from "../Atoms/Button";
 import { useEffect } from "react";
+import ProfileImage from "../Atoms/ProfileImage";
 
-const ModalWindow = styled.div``;
+const ModalWindow = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  border: 1px solid black;
+  padding-top: 20px;
+  width: 290px;
+  height: 220px;
+  //padding: 20px;
+  z-index: 999; /* 다른 요소들보다 위로 배치 */
+  display: flex;
+  flex-direction: column;
+  // justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+`;
 
-const ModalContainer = styled.div``;
+const CancleQ = styled.div`
+  height: 22px;
+  width: 230px;
+  text-align: center;
+  margin-top: 15px;
+  font-size: 12px;
+  border-bottom: 1px solid #ccc;
+`;
+const Cancle = styled.div`
+  height: 22px;
+  width: 230px;
+  text-align: center;
+  font-size: 14px;
+  margin-top: 15px;
+  cursor: pointer;
+  color: red;
+  border-bottom: 1px solid #ccc;
+`;
+const Back = styled.div`
+  height: 22px;
+  width: 230px;
+  text-align: center;
+  font-size: 14px;
+  margin-top: 15px;
+  cursor: pointer;
+`;
+const Border = styled.div`
+  border-top: 1px solid #ccc;
+`;
 
 // 코드 출처 https://joonfluence.tistory.com/m/657
 // https://im-developer.tistory.com/201
@@ -16,8 +62,8 @@ const ModalContainer = styled.div``;
  * 그리고 useEffect의 반환값은 컴포넌트가 삭제될때 실행되는 함수인데, 모달이 사라지면 스크롤을 다시 허용한다.
  */
 
-export default function ModalBottom(props) {
-  const { toggleModal } = props;
+export default function ModalMiddle(props) {
+  const { toggleModal, insertdeleteFollow } = props;
 
   let scrollPosition = 0;
 
@@ -39,12 +85,11 @@ export default function ModalBottom(props) {
 
   return (
     <ModalWindow>
-      <ModalContainer>
-        <Button>프로필 수정</Button>
-        <Button>로그아웃</Button>
-        <Button>회원탈퇴</Button>
-        <Button onClick={toggleModal}>취소</Button>
-      </ModalContainer>
+      <ProfileImage src={props.src} size={"90px"}></ProfileImage>
+
+      <CancleQ>{props.nickname}님의 팔로우를 취소하시겠어요?</CancleQ>
+      <Cancle onClick={insertdeleteFollow}>팔로우 취소</Cancle>
+      <Back onClick={toggleModal}>취소</Back>
     </ModalWindow>
   );
 }
