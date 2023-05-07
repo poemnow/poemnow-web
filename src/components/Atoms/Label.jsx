322b725d4b5a63f2d054716a04f8f5e4a6f40be3
jiwoob <jiwoo9138@gmail.com>
2023-05-07 17:07:35 +0900

    feature: label 추가, heroimg 추가

--- src/components/Atoms/Label.jsx
+++ src/components/Atoms/Label.jsx
@@ -1,14 +1,6 @@
 import styled from "styled-components";
 
 const LabelStyled = styled.label`
-<<<<<<< Updated upstream
-  font-size: 11px;
-  color: ${(props) => props.color};
-`;
-
-export default function Label({ children, color }) {
-  return <LabelStyled color={color}>{children}</LabelStyled>;
-=======
   font-size: var(--mobile-label);
   color: ${(props) => props.color};
   font-weight: ${(props) => (props.weight === light ? 300 : 500)};
@@ -20,5 +12,4 @@ export default function Label({ children, color, weight }) {
       {children}
     </LabelStyled>
   );
->>>>>>> Stashed changes
 }
