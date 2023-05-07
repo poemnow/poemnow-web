3ed535c589e3103df05aa79e8202c683fe55233f
jiwoob <114009384+jiwoob@users.noreply.github.com>
2023-05-07 17:09:13 +0900

    Gdm 35 main like (#39)
    * feature: component - organisms - Header, HeaderMain 추가
    
    * feature: font weight global css 적용
    
    * feature: label 추가, heroimg 추가

--- src/styles/global.js
+++ src/styles/global.js
@@ -5,30 +5,23 @@ import "../fonts/pretendard-subset.css";
 const GlobalVariableStyle = createGlobalStyle`
 :root {
   // font size
-  --mobile-h1: 42px;
-  --mobile-h2: 36px;
-  --mobile-h3-bold: 24px;
-  --mobile-h3-medium: 24px;
-  --mobile-h4-bold: 20px;
-  --mobile-h4-medium: 20px;
-  --mobile-h5-bold: 16px;
-  --mobile-h5-medium: 16px;
-  --mobile-h5-light: 16px;
+  --mobile-h1: 42px; 
+  --mobile-h2: 36px; 
+  --mobile-h3: 24px;
+  --mobile-h4: 20px;
+  --mobile-h5: 16px;
   --mobile-h6: 12px;
   --mobile-p: 12px;
-  --mobile-p-bold: 12px;
-  --mobile-p-light: 12px;
   --mobile-text: 14px;
-  --mobile-text: 14px;
-  --mobile-label-medium: 11px;
-  --mobile-label-light: 11px;
-  --mobile-text-light: 14px;
+  --mobile-label: 11px;
 
   // color
   --primary: #1F6313;
   --primary2: #C1E5C5;
   --secondary: #EEE9CD;
   --background: #FFFFFF;
+  --red: red;
+  --blue: blue;
   --gray-50: #f9fafb;
   --gray-100: #f3f4f6;
   --gray-200: #e5e7eb;
