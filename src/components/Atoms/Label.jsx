65b9e21060e3d8c8155d9d26fe25b0944b1e850b
jiwoob <jiwoo9138@gmail.com>
2023-05-07 17:05:14 +0900

    feature: font weight global css 적용

--- src/components/Atoms/Label.jsx
+++ src/components/Atoms/Label.jsx
@@ -1,10 +1,24 @@
 import styled from "styled-components";
 
 const LabelStyled = styled.label`
+<<<<<<< Updated upstream
   font-size: 11px;
   color: ${(props) => props.color};
 `;
 
 export default function Label({ children, color }) {
   return <LabelStyled color={color}>{children}</LabelStyled>;
+=======
+  font-size: var(--mobile-label);
+  color: ${(props) => props.color};
+  font-weight: ${(props) => (props.weight === light ? 300 : 500)};
+`;
+
+export default function Label({ children, color, weight }) {
+  return (
+    <LabelStyled color={color} weight={weight}>
+      {children}
+    </LabelStyled>
+  );
+>>>>>>> Stashed changes
 }
